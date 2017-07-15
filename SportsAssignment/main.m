f6a31bdd93112f1172d5765483e047f59c4ed85e
Shahzaib Ather <shahzaibather7@github.ca>
2017-07-15 15:05:05 -0600

    Initial Commit

---
+++ SportsAssignment/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SportsAssignment
+//
+//  Created by Mohammad Shahzaib Ather on 2017-07-15.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
